a0c5f4774c4374f1931660b9ce5ac1209886291e
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-07-28 20:08:57 -0700

    create saved list and add id to html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
 // BRANCH 2 TEST
+var formEl = $('#search');
+var treeInputEl = $('#tree-input');
+
+var searchlistDisplay = $('#saved');
+
+var printTreeData = function (tree) {
+  
+  // add a class 
+  var cardName = $('<li>').addClass('search-list').text(tree);
+
+  searchlistDisplay.append(cardName);
+};
+
+var handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  var treeInput = treeInputEl.val();
+
+  if (!treeInput) {
+    console.log('You need to fill out the form!');
+    return;
+  }
+
+  printTreeData(treeInput);
+
+  // reset form
+  treeInputEl.val('');
+};
+
+formEl.on('submit', handleFormSubmit);
+  
+
+
+
+// grab a reference to the input field and store its value in a variable.
